30e0a2408d79a5782a661484798d98f36497c640
akshayakky <akshaysmpt01@gmail.com>
2020-02-13 15:18:30 +0530

    REFACTOR : Indentation

--- functions/numberPalindrome.sh
+++ functions/numberPalindrome.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -x
 
-read -p " Enter number : " numbers
+read -p " Enter number : " number
 
 #FUNCTION TO CHECK IF NUMBER IS PALINDROME
 function isPalindrome(){
-sum=0;
-number=$1
-while [ $number -gt 0 ]
-do
-	remainder=$((number%10));
-	sum=$((sum*10+remainder));
-	number=$((number/10));
-done
+	local sum=0;
+	local number=$1
+	while [ $number -gt 0 ]
+	do
+		remainder=$((number%10));
+		sum=$((sum*10+remainder));
+		number=$((number/10));
+	done
 
-if [ $sum -eq $1 ]
-then
-	echo "Palindrome"
-else
-	echo "Not Palindrome "
-fi
-}
+	if [ $sum -eq $1 ]
+	then
+		echo "$1 is Palindrome"
+	else
+	echo "$1 is Not Palindrome "
+	fi
+	}
 
-echo "$( isPalindrome $numbers )"
+echo "$( isPalindrome $number )"
