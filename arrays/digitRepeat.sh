0e33e182d4d169576e2efc0212d345f9cf32dbe3
akshayakky <akshaysmpt01@gmail.com>
2020-02-14 10:27:28 +0530

    REFACTOR : Indentation

--- arrays/digitRepeat.sh
+++ arrays/digitRepeat.sh
@@ -2,7 +2,7 @@
 
 #CHECK FOR NUMBERS BETWEEN 0 AND 100 THAT HAVE SAME DIGITS
 count=0
-for (( counter=0;counter<100;counter++ ))
+for (( counter=0; counter<100; counter++ ))
 do
 	if [ $(($counter%10)) -eq $(($counter/10)) -a $(($counter/10)) -ne 0 ]
 	then
