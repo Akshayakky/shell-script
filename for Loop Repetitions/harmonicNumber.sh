1eed4df552eb6c343ce142fdef8393b5143d0ed5
akshayakky <akshaysmpt01@gmail.com>
2020-02-14 11:01:19 +0530

    REFACTOR : Indentation

--- for Loop Repetitions/harmonicNumber.sh
+++ for Loop Repetitions/harmonicNumber.sh
@@ -4,7 +4,7 @@ read -p "Enter any number : " number;
 harmonic=0;
 
 #CALCULATE HARMONIC NUMBER
-for (( counter=1;counter<=$number;counter++ ))
+for (( counter=1; counter<=$number; counter++ ))
 do
 	#USING SCALE AND BC TO LIMIT THE OUTPUT TO 2 DECIMAL PLACES
 	harmonic=$(echo "scale=2;$harmonic + 1/$counter" | bc );
